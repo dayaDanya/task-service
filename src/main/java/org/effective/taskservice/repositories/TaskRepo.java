a3fe8be93b1ea54a8b759d98bc4fdf1cb9623446
Данил Гончаров <singlearity1@gmail.com>
2023-12-28 18:52:04 +0300

    добавил возможность пагинации с помощью slice @query

--- src/main/java/org/effective/taskservice/repositories/TaskRepo.java
+++ src/main/java/org/effective/taskservice/repositories/TaskRepo.java
@@ -4,6 +4,7 @@ import org.effective.taskservice.domain.models.Task;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +16,8 @@ import java.util.List;
 public interface TaskRepo extends JpaRepository<Task, Long> {
     Slice<Task> findByAuthorId(long id, Pageable pageable);
     Slice<Task> findByPerformerId( long id, Pageable pageable);
+    @Query(
+            value = "select * from entities.task",
+            nativeQuery = true)
     Slice<Task> findAllSlice(Pageable pageable);
 }
